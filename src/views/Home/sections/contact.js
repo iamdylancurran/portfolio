317a1d418a39c6afc2ee1f30f1f79b7ba44f3628
Dylan Curran <dcurran@vft-solutions.com>
2020-09-14 15:30:59 -0400

    contact form

--- src/views/Home/sections/contact.js
+++ src/views/Home/sections/contact.js
@@ -1,6 +1,34 @@
 import React from 'react';
+import styled from 'styled-components';
 import { Grid, Row } from '../../../components/layout/Grid';
 import { Header } from '../../../components/typography/Typography';
+import Button from '../../../components/misc/Button';
+
+const Input = styled.input`
+  width: 100%;
+  padding: 0.5rem 1rem;
+  margin: 1rem 0;
+  box-sizing: border-box;
+  font-family: 'Fira Sans';
+
+  &:focus {
+    outline: 1px solid ${({ theme }) => theme.colors.primary.main};
+  }
+`;
+
+const TextArea = styled.textarea`
+  width: 100%;
+  min-height: 150px;
+  padding: 1rem 1.5rem;
+  margin: 1rem 0;
+  font-family: 'Fira Sans';
+  box-sizing: border-box;
+  resize: none;
+
+  &:focus {
+    outline: 1px solid ${({ theme }) => theme.colors.primary.main};
+  }
+`;
 
 const Contact = () => {
   return (
@@ -8,32 +36,23 @@ const Contact = () => {
       <Header color="textPrimary" align="center">
         Contact
       </Header>
-      <Grid maxWidth="lg" direction="column" flexWrap="wrap">
+      <Grid maxWidth="sm" direction="column" flexWrap="wrap">
         <form name="contact" method="post">
           <input type="hidden" name="form-name" value="contact" />
           <Row>
-            <p>
-              <label>
-                Name: <input type="text" name="name" />
-              </label>
-            </p>
+            <label htmlFor="name">Name:</label>
+            <Input id="name" type="text" name="name" required />
           </Row>
           <Row>
-            <p>
-              <label>
-                Email: <input type="text" name="email" />
-              </label>
-            </p>
+            <label htmlFor="email">Email:</label>
+            <Input type="email" name="email" required />
           </Row>
           <Row>
-            <p>
-              <label>
-                Message: <textarea type="text" name="message" />
-              </label>
-            </p>
+            <label htmlFor="message">Message:</label>
+            <TextArea type="text" name="message" required />
           </Row>
           <Row>
-            <button type="submit">Send</button>
+            <Button type="submit">Send</Button>
           </Row>
         </form>
       </Grid>
